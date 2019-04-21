4de27c231c8ef6435d1fd00484a07b9b4cc08430
Chris George <chrisg1622@gmail.com>
2019-04-21 11:18:38 +0100

    added tokenizer

--- hpbot/internal/spacy_tokenizer.py
+++ hpbot/internal/spacy_tokenizer.py
@@ -1,10 +1,11 @@
 import spacy
+import os
 
 
 class SpacyTokenizer:
 
     def __init__(self):
-        self.tagger = spacy.load('en_core_web_sm')
+        self.tagger = spacy.load(os.environ['SPACY_PATH'])
 
     def tokenize(self, text):
         return [token.text for token in self.tagger(text)]
