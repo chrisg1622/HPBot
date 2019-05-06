d5eebb2f40b757870469adacb166a14139831e69
Chris George <chrisg1622@gmail.com>
2019-05-06 14:20:51 +0100

    updated custom callbacks for checkpointing and sampling after each epoch

--- hpbot/internal/callbacks/model_checkpoint.py
+++ hpbot/internal/callbacks/model_checkpoint.py
@@ -1,23 +1,30 @@
-from tensorflow.python.keras.callbacks import Callback
+import inspect
+import json
+from tensorflow.python import keras
 
 
-class SequenceSamplingCallback(Callback):
+class ModelCheckpointCallback(keras.callbacks.Callback):
 
-    def __init__(self, sequence_length, samples=1, greedy_sequence=False, name=None):
-        super(SequenceSamplingCallback, self).__init__()
-        self.sequence_length = sequence_length
-        self.samples = samples
-        self.greedy_sequence = greedy_sequence
-        self.name = name or 'sample'
+    def __init__(self, model_directory, model_name, verbose=0):
+        super(ModelCheckpointCallback, self).__init__()
+        self.model_directory = model_directory
+        self.model_name = model_name
+        self.verbose = verbose
+        self.model_filepath = f'{self.model_directory}/{self.model_name}.h5'
 
-    def print_sample(self):
-        for i in range(self.samples):
-            sample = self.model.sample_sequence(size=self.sequence_length, greedy=self.greedy_sequence)
-            print(f'{self.name} {i}: {sample}')
+    @property
+    def model_params(self):
+        return {k: v for k, v in self.model.__dict__.items() if k in inspect.signature(self.model.__class__).parameters.keys()}
 
     def on_train_begin(self, logs=None):
-        self.print_sample()
+        for name, value in self.model_params.items():
+            if isinstance(value, keras.layers.Layer):
+                path = f'{self.model_directory}/{name}.json'
+                json.dump(value.get_config(), open(path, 'w'))
+                if self.verbose > 0:
+                    print(f'Saved layer params to {path}')
 
     def on_epoch_end(self, epoch, logs=None):
-        self.print_sample()
-
+        self.model.save_weights(self.model_filepath, overwrite=True, save_format='h5')
+        if self.verbose > 1:
+            print(f'Saved model weights to {self.model_filepath}')
