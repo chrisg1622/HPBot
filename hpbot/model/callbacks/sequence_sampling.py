e63cb0f182379d8f44ce85946b4e78a7748f2b50
Chris George <chrisg1622@gmail.com>
2019-05-19 17:21:24 +0100

    added logging and removed old script

--- hpbot/model/callbacks/sequence_sampling.py
+++ hpbot/model/callbacks/sequence_sampling.py
@@ -3,17 +3,22 @@ from tensorflow.python.keras.callbacks import Callback
 
 class SequenceSamplingCallback(Callback):
 
-    def __init__(self, max_sequence_length, samples=1, greedy_sequence=False, name=None):
+    def __init__(self, max_sequence_length, samples=1, greedy_sequence=False, name=None, logger=None):
         super(SequenceSamplingCallback, self).__init__()
         self.max_sequence_length = max_sequence_length
         self.samples = samples
         self.greedy_sequence = greedy_sequence
         self.name = name or 'sample'
+        self.logger = logger
 
     def print_sample(self):
         for i in range(self.samples):
             sample = self.model.sample_sequence(words=None, greedy=self.greedy_sequence, max_sample_length=self.max_sequence_length)
-            print(f'\n{self.name} {i}: {sample}\n')
+            message = f'\n{self.name} {i}: {sample}\n'
+            if self.logger is not None:
+                self.logger.info(msg=message)
+            else:
+                print(message)
 
     def on_train_begin(self, logs=None):
         self.print_sample()
--- hpbot/scripts/train_hpbot.py
+++ hpbot/scripts/train_hpbot.py
@@ -1,12 +1,15 @@
 import fire
 import json
+import numpy as np
 import tensorflow as tf
 from tensorflow.python import keras
 from hpbot.model.spacy_tokenizer import SpacyTokenizer
 from hpbot.model.encoder import Encoder
 from hpbot.model.hpbot import HPBot
+from hpbot.model.custom_loss import CustomLoss
 from hpbot.model.callbacks.sequence_sampling import SequenceSamplingCallback
 from hpbot.model.callbacks.model_checkpoint import ModelCheckpointCallback
+from hpbot.model.logger import Logger
 from hpbot.store.txt_novel_retriever import TxtNovelRetriever
 from hpbot.store.novel_sequence_generator import NovelSequenceGenerator
 
@@ -17,40 +20,38 @@ sequence_generator = NovelSequenceGenerator(tokenizer=spacy_tokenizer)
 repository_path = '/Users/cgeorge/Git/HPBot'
 model_name = 'HPBot'
 optimizer = tf.optimizers.Adam(learning_rate=0.007)
-loss_function = tf.losses.SparseCategoricalCrossentropy(from_logits=False)
-metrics = [keras.metrics.SparseCategoricalAccuracy()]
+loss_function = CustomLoss()
+metrics = []
+logger = Logger(name='HPBot', file_path=f'{repository_path}/training.log')
+
+save_dir = f'{repository_path}/models/global'
+tensorboard_dir = f'{repository_path}/tensorboard/global'
+vocabulary = json.load(open(f'{repository_path}/models/word2vec_vocab.json'))
+embeddings = np.load(f'{repository_path}/models/word2vec_vectors.npy')
 
 
-def main(sequence_size=5, batch_size=64, epochs=50, novel_number=1, restore_model=True):
-    save_dir = f'{repository_path}/models/novel{novel_number}'
-    tensorboard_dir = f'{repository_path}/tensorboard/novel{novel_number}'
-    novel = txt_novel_retriever.retrieve_novel(f'{repository_path}/Data/HP{novel_number}.txt')
-    if restore_model:
-        encoder = Encoder.from_config(config=json.load(open(f'{save_dir}/Encoder.json', 'r')))
-    else:
-        vocabulary = sequence_generator.get_novel_vocabulary(novels=[novel])
-        encoder = Encoder(vocabulary=vocabulary, embedding_dimension=300)
-    hp_bot = HPBot(encoder=encoder, sequence_size=sequence_size)
-
-    token_sequences, target_tokens = sequence_generator.get_novel_training_ngrams(novels=[novel], n=sequence_size)
-    target_indices = hp_bot.get_target_indices(tokens=target_tokens)
+def main(batch_size=64, epochs=3, batches_per_epoch=None, restore_model=True):
+    sentence_tokens = json.load(open(f'{repository_path}/data/sentence_tokens.json', 'r'))
+    encoder = Encoder(vocabulary=vocabulary, hidden_size=512, pretrained_embeddings=embeddings)
+    hp_bot = HPBot(encoder=encoder)
+    input_generator = sequence_generator.get_training_sequence_generator(sentence_tokens=sentence_tokens, target_wrapper_fn=hp_bot.get_sentence_token_indices)
     hp_bot.compile(optimizer=optimizer, loss=loss_function, metrics=metrics)
-    _ = hp_bot(token_sequences[:1])
+    _ = hp_bot(np.array([['', '', '']]))
     if restore_model:
         hp_bot.load_weights(filepath=f'{save_dir}/{model_name}.h5')
     callbacks = [
-        ModelCheckpointCallback(model_directory=save_dir, model_name=model_name, verbose=1),
-        keras.callbacks.TensorBoard(log_dir=tensorboard_dir, profile_batch=False),
-        SequenceSamplingCallback(sequence_length=30, greedy_sequence=False, name='non-greedy sample'),
-        SequenceSamplingCallback(sequence_length=30, greedy_sequence=True, name='greedy sample')
+        ModelCheckpointCallback(model_directory=save_dir, model_name=model_name, as_pickle=True, verbose=1),
+        keras.callbacks.TensorBoard(log_dir=tensorboard_dir, write_graph=True, update_freq='batch', profile_batch=False),
+        SequenceSamplingCallback(max_sequence_length=30, greedy_sequence=False, name='non-greedy sample', logger=logger),
+        SequenceSamplingCallback(max_sequence_length=30, greedy_sequence=True, name='greedy sample', logger=logger)
     ]
+    batches_per_epoch = batches_per_epoch or len(sentence_tokens) // batch_size
     hp_bot.fit(
-        x=token_sequences,
-        y=target_indices,
+        x=input_generator,
         batch_size=batch_size,
         epochs=epochs,
-        workers=4,
-        validation_split=0.1,
+        steps_per_epoch=batches_per_epoch,
+        workers=0,
         callbacks=callbacks
     )
 
