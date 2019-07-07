87becb6c0784e1c31242e0067421d819b44843f2
Chris George <chrisg1622@gmail.com>
2019-07-07 14:11:00 +0100

    added flow class and update training data script

---
+++ language_model/model/tasks/flow.py
@@ -0,0 +1,10 @@
+from prefect import context
+from prefect import Flow as PrefectFlow
+from prefect.utilities.notifications import slack_notifier
+
+
+class Flow(PrefectFlow):
+
+    def __init__(self, name, schedule=None):
+        super().__init__(name=name, schedule=schedule, state_handlers=[slack_notifier])
+        context['flow'] = self
--- language_model/scripts/build_training_data.py
+++ language_model/scripts/build_training_data.py
@@ -5,6 +5,7 @@ from language_model.model.tasks.get_novel_sentence_tokens import GetNovelSentenc
 from language_model.model.tasks.train_word2vec_embeddings import TrainWord2VecEmbeddings
 from language_model.model.spacy_tokenizer import SpacyTokenizer
 from language_model.store.txt_novel_retriever import TxtNovelRetriever
+from language_model.model.tasks.flow import Flow
 
 
 def main(base_directory):
@@ -19,8 +20,11 @@ def main(base_directory):
         min_doc_frequency=1,
         iterations=20
     )
-    sentence_tokens = get_novel_sentence_tokens.run()
-    train_word2vec_embeddings.run(sentence_tokens=sentence_tokens)
+    flow = Flow(name='build_training_data')
+    flow.add_task(task=get_novel_sentence_tokens)
+    flow.add_task(task=train_word2vec_embeddings)
+    train_word2vec_embeddings.set_upstream(task=get_novel_sentence_tokens, flow=flow)
+    train_word2vec_embeddings.bind(sentence_tokens=get_novel_sentence_tokens)
 
 
 if __name__ == '__main__':
