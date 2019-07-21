957f660246149ca26df4b1bcf5a178a19911ebbd
Chris George <chrisg1622@gmail.com>
2019-07-21 15:39:54 +0100

    updated task class

--- language_model/model/tasks/task.py
+++ language_model/model/tasks/task.py
@@ -6,6 +6,7 @@ from language_model.model.logger import Logger
 class Task(PrefectTask):
 
     def __init__(self, base_directory=None):
+        self.base_directory = base_directory
         super().__init__(name=self.__class__.__name__, state_handlers=[slack_notifier], skip_on_upstream_skip=False)
         self.logger = Logger(name=self.name, file_path=f'{base_directory}/{self.name}.log' if base_directory is not None else None)
 
