e30bfbb068aebf583d7650613786878aeef2f7e8
Chris George <chrisg1622@gmail.com>
2019-07-21 15:39:40 +0100

    refactored sentence token retrieval task

--- language_model/model/tasks/get_novel_sentence_tokens.py
+++ language_model/model/tasks/get_novel_sentence_tokens.py
@@ -1,15 +1,23 @@
 import glob
 import json
+from language_model.data.constants import START_TOKEN
+from language_model.data.constants import EOS_TOKEN
 from language_model.model.tasks.task import Task
 
 
 class GetNovelSentenceTokens(Task):
 
+    TRAINING_SEQUENCES_FILE_NAME = 'all_novels_sentence_tokens.json'
+    VOCABULARY_FILE_NAME = 'vocabulary.json'
+
     def __init__(self, base_directory, novel_retriever, tokenizer):
-        super().__init__()
-        self.base_directory = base_directory
+        super().__init__(base_directory=base_directory)
         self.novel_retriever = novel_retriever
         self.tokenizer = tokenizer
+
+    @staticmethod
+    def pad_tokens(tokens):
+        return [START_TOKEN] + tokens + [EOS_TOKEN]
 
     def run(self):
         sentence_tokens = []
@@ -18,10 +26,12 @@ class GetNovelSentenceTokens(Task):
             self.log(f'Processing {novel.title}')
             sentence_tokens.append(self.tokenizer.get_tokens(text=novel.title))
             for chapter in novel.chapters:
-                sentence_tokens.append(self.tokenizer.get_tokens(text=chapter.title))
-                sentence_tokens.extend(self.tokenizer.get_sentence_tokens(text=chapter.text))
+                sentence_tokens.append(self.pad_tokens(tokens=self.tokenizer.get_tokens(text=chapter.title)))
+                for tokens in self.tokenizer.get_sentence_tokens(text=chapter.text):
+                    padded_tokens = self.pad_tokens(tokens=tokens)
+                    sentence_tokens.append(padded_tokens)
         vocab = sorted({x for sentence in sentence_tokens for x in sentence})
         self.log(f'Saving {len(sentence_tokens)} with a total of {len(vocab)} words')
-        json.dump(vocab, open(f'{self.base_directory}/vocabulary.json', 'w'))
-        json.dump(sentence_tokens, open(f'{self.base_directory}/all_novels_sentence_tokens.json', 'w'))
+        json.dump(vocab, open(f'{self.base_directory}/{self.VOCABULARY_FILE_NAME}', 'w'))
+        json.dump(sentence_tokens, open(f'{self.base_directory}/{self.TRAINING_SEQUENCES_FILE_NAME}', 'w'))
         return sentence_tokens
